40b69b2fc89ae8591c7d800e53baacaedd74a113
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-12-04 18:47:56 +0900

    スタイルイジイジ

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="container">
-    <div class="columns">
-      <div class="column is-half ">
+    <div v-for="i in 9" key="{{ i }}">
+      <div v-if="i % 3 === 0" class="columns">
+          <div class="column">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Primary</p>
+                <button class="delete" aria-label="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -20,18 +32,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
+  .container {
+    padding-top: 5vh;
   }
 </style>
