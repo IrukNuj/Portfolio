8d0b6d0f134e78ef770d67ffa6c4fbcf9ff634be
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-12-05 18:52:49 +0900

    カードのトランジション

--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="column">
-    <div class="card">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-        </figure>
-      </div>
+    <transition appear>
+      <div v-show="show" class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+          </figure>
+        </div>
 
-      <div class="card-content">
-        <div class="media">
-          <div class="media-content">
-            <p class="title is-5">おもいだしったー</p>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <p class="title is-5">おもいだしったー</p>
+            </div>
+          </div>
+
+          <div class="content">
+            Railsで簡単にOauth認証を実装し、ログインユーザーの過去の呟きを思い出せるアプリを作成。
+            <br>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
           </div>
         </div>
-
-        <div class="content">
-          Railsで簡単にOauth認証を実装し、ログインユーザーの過去の呟きを思い出せるアプリを作成。
-          <br>
-          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-        </div>
+        <button @click="show = !show">きりきり</button>
       </div>
-
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -30,7 +32,7 @@ export default {
   name: 'WorkCard',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      show: true
     }
   }
 }
@@ -40,5 +42,20 @@ export default {
 <style scoped>
   .container {
     padding-top: 5vh;
+  }
+  .v-enter-active, .v-leave-active {
+    transition: opacity 1s;
+  }
+  .v-enter {
+    opacity: 0;
+  }
+  .v-enter-to {
+    opacity: 1;
+  }
+  .v-leave {
+    opacity: 1;
+  }
+  .v-leave-to {
+    opacity: 0;
   }
 </style>
