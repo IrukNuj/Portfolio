c7be9d9b54c6495e8f8a819d312e0e34790994dd
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-12-07 18:06:49 +0900

    npm run dev for github

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: '/Portfolio/',
     proxyTable: {},
 
     // Various Dev Server settings
