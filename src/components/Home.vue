69488599bebea5056f6f4b670e8ede93482658af
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-01-07 14:06:19 +0900

    ダル子ちゃんの表示と簡単なようこそ字幕を表示

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,12 @@
 <template>
+  <div class="box home-content level">
+    <div class="level-left">
+      <h1 class="subtitle">Welcome to Iruk's site!</h1>
+    </div>
+    <div class="daru-girl-image level-right">
+      <img src="../assets/daru-girl_home.png"/>
+    </div>
+  </div>
 
 </template>
 
@@ -14,5 +22,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .home-content{
+    height: 80vh;
+  }
+  .daru-girl-image{
+    width: 80%;
+    height: 80%;
+  }
 </style>
