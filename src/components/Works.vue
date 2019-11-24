2e65757ba8753156b813e7f9fe67cf412446aacb
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-11-24 13:25:32 +0900

    :bug: Fix columns.

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -80,6 +80,23 @@
         </div>
       </div>
     </div>
+
+    <div class="columns">
+      <div v-for="work in works" :key="work.id" >
+        <div v-if="work.id >= 16 && work.id <= 18">
+          <WorkCard
+            :id = "work.id"
+            :name = "work.name"
+            :description = "work.description"
+            :url = "work.url"
+            :path = "work.path"
+            :tags = "work.tag"
+          >
+          </WorkCard>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
